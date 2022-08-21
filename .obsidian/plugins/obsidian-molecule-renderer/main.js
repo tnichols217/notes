83a2599a6466cfb4cf9c87a5be7075794d5f804f
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-22 00:23:51 +0800

    vault backup: 2022-08-22 00:23:51

--- .obsidian/plugins/obsidian-molecule-renderer/main.js
+++ .obsidian/plugins/obsidian-molecule-renderer/main.js
@@ -120,7 +120,10 @@ var DEFAULT_SETTINGS = {
 var ObsidianMoleculeRenderer = class extends import_obsidian2.Plugin {
   getMolecule(src) {
     return __async(this, null, function* () {
-      return JSON.parse(yield (0, import_obsidian2.request)({ url: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/" + src + "/property/MolecularFormula/JSON" }));
+      return JSON.parse((yield (0, import_obsidian2.request)({ url: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/" + src + "/property/MolecularFormula/JSON" }).catch((err) => {
+        console.error(err);
+        console.log(src);
+      })) || "");
     });
   }
   moleculeNotFound(src, el) {
@@ -130,7 +133,10 @@ var ObsidianMoleculeRenderer = class extends import_obsidian2.Plugin {
       heading = el.createEl("h2");
       heading.innerText = "Similar Chemicals include:";
       console.log(src);
-      let suggestions = JSON.parse((yield (0, import_obsidian2.request)({ url: "https://pubchem.ncbi.nlm.nih.gov/rest/autocomplete/compound/" + src }).catch(console.error)) || "").dictionary_terms.compound;
+      let suggestions = JSON.parse((yield (0, import_obsidian2.request)({ url: "https://pubchem.ncbi.nlm.nih.gov/rest/autocomplete/compound/" + src }).catch((err) => {
+        console.error(err);
+        console.log(src);
+      })) || "").dictionary_terms.compound;
       let list = el.createEl("ol");
       for (let i of suggestions) {
         let item = list.createEl("li");
